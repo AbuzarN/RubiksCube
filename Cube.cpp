a504231ce7b9ea072a783c1963ecda298843ff35
AbuzarN <118425351+AbuzarN@users.noreply.github.com>
2023-10-05 00:16:13 -0500

    Update Cube.cpp

--- Cube.cpp
+++ Cube.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 using namespace std;
+  char F1 [3][3] =   {{'W','W','W'},
+                      {'W','W','W'},
+                      {'W','W','W'}};
+
+  char F2 [3][3] =   {{'B','B','B'},
+                      {'B','B','B'},
+                      {'B','B','B'}};
+
+  char F3 [3][3] =   {{'O','O','O'},
+                      {'O','O','O'},
+                      {'O','O','O'}};
+
+  char F4 [3][3] =   {{'G','G','G'},
+                      {'G','G','G'},
+                      {'G','G','G'}};
+
+  char F5 [3][3] =   {{'R','R','R'},
+                      {'R','R','R'},
+                      {'R','R','R'}};
+
+  char F6 [3][3] =   {{'Y','Y','Y'},
+                      {'Y','Y','Y'},
+                      {'Y','Y','Y'}};
+
+
 
 int main() 
 {
-  int F1 [3][3][3] = {{W,W,W}{W,W}{}}
-  return 0
+  return 0;
 }
+
