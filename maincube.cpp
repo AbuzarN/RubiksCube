fe1496209d7d0a1eda579ca90c2a6708b16a10ee
Muhammad Qasim Zaidi <qasimzaidi16@gmail.com>
2024-01-10 22:42:05 -0600

    syncing changes

--- maincube.cpp
+++ maincube.cpp
@@ -6,27 +6,16 @@
 #include "Rubix.cpp"
 #include "Rubix.hpp"
 
-
-
-
 int main()
 {
 
     Rubix cube;
-    cube.make_cube('W','G','Y','O','R','B');
+    cube.make_cube('W', 'G', 'Y', 'O', 'R', 'B');
     std::cout << "Fresh Cube!" << std::endl;
     cube.display_cube();
     cube.scramblecube();
-    cube.white_cross();
-    cube.white_corners();
-    cube.second_layer();
-    cube.yellow_cross();
-    cube.yellow_edges();
-    cube.yellow_corners();
-    cube.final_step();
+
     cube.solution_rotations();
-   
-   
+
     return 0;
 }
-
